3f353411a3a66470582911b634237f97e11ede4a
pawitchaya <boss@cmu.ac.th>
2024-12-26 21:48:57 +0700

    lap12_3.cpp completed

--- lab12_3.cpp
+++ lab12_3.cpp
@@ -15,3 +15,26 @@ int main(){
 	
 	return 0;
 }
+int fibonacci(int x) {
+    if (x == 0) {
+        return 0;
+    } else if (x == 1) {
+        return 1;
+    } else {
+        return fibonacci(x - 1) + fibonacci(x - 2);
+    }
+}
+
+int gcd(int x, int y) {
+    if (y == 0) {
+        return x;
+    } else {
+        return gcd(y, x % y);
+    }
+}
+
+void mySwap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
