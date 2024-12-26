5f8caeaeb97ad15cafde2e6dd10a2fb051e5f7b2
pawitchaya <boss@cmu.ac.th>
2024-12-26 21:48:52 +0700

    lap12_2.cpp completed

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,9 +1,30 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int x);
+int gcd(int x, int y);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+int fibonacci(int x) {
+    if (x == 0) {
+        return 0;
+    } else if (x == 1) {
+        return 1;
+    } else {
+        return fibonacci(x - 1) + fibonacci(x - 2);
+    }
+}
+
+int gcd(int x, int y) {
+    if (y == 0) {
+        return x;
+    } else {
+        return gcd(y, x % y);
+    }
 }
